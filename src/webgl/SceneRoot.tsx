8ab069fe18331261aa3ece877462eeb9a9a563c5
b8stianjaja <bastianpuga3@gmail.com>
2025-11-23 21:21:45 -0300

    Restart

--- src/webgl/SceneRoot.tsx
+++ src/webgl/SceneRoot.tsx
@@ -1,13 +1,11 @@
 import { Canvas, useLoader } from '@react-three/fiber'
-import { Stars, Environment } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
 import * as THREE from 'three'
 import { useStore } from '@state/index'
 import { RGBELoader } from 'three-stdlib'
 import { useMemo } from 'react'
 
-import { CelestialSun } from './components/CelestialSun'
-import { ObsidianWater } from './components/ObsidianWater'
+
 
 function SceneContent() {
   const debug = useStore((state) => state.debug)
@@ -26,19 +24,7 @@ function SceneContent() {
 
   return (
     <>
-      {/* 3. APPLY TO SKY: Pass the loaded texture to Environment */}
-      <Environment 
-        map={texture} 
-        background={true} 
-        blur={0.5} 
-      />
 
-      <Stars radius={100} depth={50} count={2000} factor={4} saturation={0} fade speed={0.5} />
-      
-      <CelestialSun />
-      
-      {/* 4. APPLY TO WATER: Pass the same texture instance down */}
-      <ObsidianWater envMap={texture} />
 
       {debug && <Perf position="top-left" />}
     </>
