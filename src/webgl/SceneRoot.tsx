34c3621d6a3b38ab13fd9728d45834b1b05d8e74
b8stianjaja <bastianpuga3@gmail.com>
2025-11-26 02:27:22 -0300

    Clean

--- src/webgl/SceneRoot.tsx
+++ src/webgl/SceneRoot.tsx
@@ -3,28 +3,22 @@ import { Perf } from 'r3f-perf'
 import * as THREE from 'three'
 import { useStore } from '@state/index'
 import { RGBELoader } from 'three-stdlib'
-import { useMemo } from 'react'
-
-
+import { useMemo, Suspense } from 'react'
+import { Environment } from '@react-three/drei'
 
 function SceneContent() {
   const debug = useStore((state) => state.debug)
 
-  // 1. MASTER LOAD: Load the HDRI once here
-  // Ensure this file exists in your 'public' folder!
-  const texture = useLoader(RGBELoader, '/hdr/mainhdr.hdr')
+  
 
-  // 2. CONFIGURE: Set up the texture settings once
-  useMemo(() => {
-    texture.mapping = THREE.EquirectangularReflectionMapping
-    texture.minFilter = THREE.LinearFilter
-    texture.magFilter = THREE.LinearFilter
-    texture.needsUpdate = true
-  }, [texture])
 
   return (
     <>
+     
+      <Environment preset='city'/>
+     
 
+     
 
       {debug && <Perf position="top-left" />}
     </>
@@ -36,14 +30,16 @@ export default function SceneRoot() {
     <Canvas
       className="canvas-layer"
       dpr={[1, 1.5]} 
-      camera={{ position: [0, 2, 14], fov: 35 }} 
+      camera={{ position: [0, 5, 10], fov: 45 }}
       gl={{ 
         toneMapping: THREE.ACESFilmicToneMapping,
-        toneMappingExposure: 0.8,
+        toneMappingExposure: 1.0,
         antialias: true 
       }}
     >
-      <SceneContent />
+      <Suspense fallback={null}>
+        <SceneContent />
+      </Suspense>
     </Canvas>
   )
 }
